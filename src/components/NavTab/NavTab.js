dde5db44307193674060067da32cb45b435b69ee
Anzhela Alexandrovich <baronina.anzhela2017@yandex.ru>
2023-09-07 17:25:26 +0300

    level-3

--- src/components/NavTab/NavTab.js
+++ src/components/NavTab/NavTab.js
@@ -5,7 +5,7 @@ function NavTab() {
         <nav className="nav-tab">
             <a href="#aboutProject" className="nav-tab__link">О проекте</a>
             <a href="#techs" className="nav-tab__link">Технологии</a>
-            <a href="#aboutMe" className="nav-tab__link">Студент</a>
+            <a href="#about-me" className="nav-tab__link">Студент</a>
         </nav >
     )
 }
